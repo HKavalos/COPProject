732054bb257a723db62fe61436c81db7451b4dcc
Raven Kim Ramos <68404591+ravenkimr@users.noreply.github.com>
2021-04-18 17:07:09 -0400

    Update project3.rb

--- project3.rb
+++ project3.rb
@@ -19,54 +19,55 @@ request["Cookie"] = "__cfduid=dd91ed6451babd79469936dfd983802bd1617112942"
 
 outfile = "games.csv"
 file = File.open(outfile,'w') 
-file.write("ID,Name,Rating,Genres,Platforms,Developer/Publisher\n")
+file.write("ID,Name,Rating,Genres,Platforms,Companies\n")
 min = 0
-max = 499
-
+max = 0
 genres_ = ""
 platforms_ = ""
 companies_ = ""
-aggregatedRating_ = "";
-
-while min <= 100000
-	query = "fields name,genres.name, platforms.name, aggregated_rating, involved_companies.company.name,age_ratings.content_descriptions.description; where id > #{min}; limit 500; sort id asc;"
+while min <= 200000
+	max = min + 500
+	query = "fields name,genres.name, platforms.name, aggregated_rating, involved_companies.company.name; where id > #{min} & id <= #{max}; limit 500;"
 	request.body = query
-
+	#p query
 	response = https.request(request)
 	res = response.read_body
 	data = JSON.parse(res)
 	data.each do |d|
-
 		genres = d["genres"]
 		if genres
 			genres.each do |g|
 				genres_ = genres_ + g["name"] + ":"	
 			end
 		end
-
 		platforms = d["platforms"]
 		if platforms
 			platforms.each do |p|
 				platforms_ = platforms_ + p["name"] + ":"
 			end
 		end
-
 		companies = d["involved_companies"]
 		if companies
 			companies.each do |c|
 				companies_ = companies_ + c["company"]["name"] + ":"
 			end
 		end
-
-
+		rating = d["aggregated_rating"]
+		if rating == ""
+			rating = "0"
+		end
+		d["name"].gsub!("\"","\'")
 		game_name = "\"#{d["name"]}\""
+		genres = "\"#{genres_[0,genres_.length-1]}\""
 		platform_names = "\"#{platforms_[0,platforms_.length-1]}\""
 		company_names = "\"#{companies_[0,companies_.length-1]}\""
-		file.write("#{d["id"]},#{game_name}, #{d["aggregated_rating"].to_f.round(2)},#{genres_[0,genres_.length-1]},#{platform_names},#{company_names}\n")
+		#file.write("#{d["id"]},#{game_name},#{d["aggregated_rating"]},#{genres},#{platform_names},#{company_names}")
+		if d["id"] != ""
+			file.puts("#{d["id"]},#{game_name},#{d["aggregated_rating"].to_f.round(2)},#{genres},#{platform_names},#{company_names}")
+		end
 		genres_ = ""
 		platforms_ = ""
 		companies_ = ""
-
 	end
 	min += 500
 end
